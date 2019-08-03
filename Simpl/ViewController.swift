4de3ad5c04ab45645486cf7f1ae5ddbc70418f26
Maxim <maxeeem@gmail.com>
2019-08-02 17:22:20 -0700

    Initial Commit

---
+++ Simpl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpl
+//
+//  Created by Maxim VT on 8/2/19.
+//  Copyright © 2019 Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
